03c3e81c6429b108cc88ad2b97f6f40b974d6160
Hemant Gupta <guptahemant65@gmail.com>
2019-07-25 16:45:23 +0530

    make it better

--- main.go
+++ main.go
@@ -2,10 +2,14 @@
 
 package main
 
+import (
+	"os"
+)
+
 func main() {
 	a := App{}
 	// You need to set your Username and Password here
 	a.Initialize("root", "abcd5520", "sia_lounge")
 
-	a.Run(":8080")
+	a.Run(":" + os.Getenv("PORT"))
 }
